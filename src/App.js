f8a1e929f0663dea091f992ba9ef59ce4fca01f7
itsmejoeyb <31454088+itsmejoeyb@users.noreply.github.com>
2019-11-17 20:24:49 -0500

    various updates

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Row, Col, Form, Button, Image } from 'react-bootstrap'
+import { Container, Row, Col, Form, Button, Image, Modal } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Logo from './2019-Legendary-Lion-Main-Full-Color.png'
+import './style.css'
 import './App.css';
+
+const dev = 'http://localhost:3200'
+const live = 'http://leaderboard-api.legendarydevelopment.net/api'
 
 function PlayerForm({addPlayer}) {
   const defaultState = {
@@ -31,14 +35,56 @@ function PlayerForm({addPlayer}) {
   }
 
   return (
-    <Form className="form" onSubmit={handleSubmit}>
+    <Form id="form" className="form" onSubmit={handleSubmit}>
+      <Form.Row>
+        <Col>
+          <Form.Group controlId="formGroupCompany">
+            <Form.Control required
+              type="text"
+              placeholder="Company Name"
+              value={company}
+              name={"company"}
+              onChange={handleChange}
+            />
+          </Form.Group>
+        </Col>
+      </Form.Row>
+      <Form.Row>
+        <Col lg={8}>
+          <Form.Group controlId="formGroupUrl">
+            <Form.Control required
+              type="text"
+              placeholder="Website"
+              value={url}
+              name={"url"}
+              onChange={handleChange}
+            />
+          </Form.Group>
+        </Col>
+        <Col lg={4}>
+          <Form.Group controlId="formGroupUrl">
+            <Form.Control required
+              type="text"
+              placeholder="Time"
+              value={time}
+              name={"time"}
+              onChange={handleChange}
+            />
+          </Form.Group>
+        </Col>
+      </Form.Row>
+      <hr/>
+      <Form.Row >
+        <Col>
+          <h5 className="pb-3">Where should we send your report?</h5>
+        </Col>
+      </Form.Row>
       <Form.Row>
         <Col>
           <Form.Group controlId="formGroupFirstName">
-            <Form.Label>First Name</Form.Label>
             <Form.Control required
               type="text" 
-              placeholder="Enter first name" 
+              placeholder="First Name" 
               value={first}
               name={"first"}
               onChange={handleChange}
@@ -47,78 +93,56 @@ function PlayerForm({addPlayer}) {
         </Col>
         <Col>
           <Form.Group controlId="formGroupLastName">
-            <Form.Label>Last Name</Form.Label>
             <Form.Control required
               type="text" 
-              placeholder="Enter last name" 
+              placeholder="Last Name" 
               value={last}
               name={"last"}
               onChange={handleChange}
             />
           </Form.Group>
         </Col>
+      </Form.Row>
+      <Form.Row>  
         <Col>
           <Form.Group controlId="formGroupEmail">
-            <Form.Label>Email</Form.Label>
             <Form.Control required
               type="text" 
-              placeholder="Enter email" 
+              placeholder="Email" 
               value={email}
               name={"email"}
               onChange={handleChange}
             />
           </Form.Group>
         </Col>
-        <Col>
-          <Form.Group controlId="formGroupCompany">
-            <Form.Label>Company Name</Form.Label>
-            <Form.Control required
-              type="text" 
-              placeholder="Enter company name" 
-              value={company}
-              name={"company"}
-              onChange={handleChange}
-            />
-          </Form.Group>
-        </Col>
-        <Col>
-          <Form.Group controlId="formGroupUrl">
-            <Form.Label>Website</Form.Label>
-            <Form.Control required
-              type="text" 
-              placeholder="Enter website" 
-              value={url}
-              name={"url"}
-              onChange={handleChange}
-            />
-          </Form.Group>
-        </Col>
-        <Col>
-          <Form.Group controlId="formGroupUrl">
-            <Form.Label>Time</Form.Label>
-            <Form.Control required
-              type="text" 
-              placeholder="Enter time" 
-              value={time}
-              name={"time"}
-              onChange={handleChange}
-            />
-          </Form.Group>
-        </Col>
       </Form.Row>
-      <Button type="submit">Submit</Button>
     </Form>
   )
 }
 
+function ordinal_suffix_of(i) {
+  var j = i % 10,
+    k = i % 100;
+  if (j === 1 && k !== 11) {
+    return i + "st";
+  }
+  if (j === 2 && k !== 12) {
+    return i + "nd";
+  }
+  if (j === 3 && k !== 13) {
+    return i + "rd";
+  }
+  return i + "th";
+}
+
 function Player({ player, index, removePlayer }) {
   return(
-    <Row className="player align-items-center text-center">
-      <Col lg={1}><h3>{index + 1}</h3></Col>
-      <Col lg={4}><h3>{player.name}</h3></Col>
-      <Col lg={5}><h3><a href={player.url} target="_blank" rel="noopener noreferrer">{player.url}</a></h3></Col>
-      <Col lg={1}><h3>{player.time}</h3></Col>
-      <Col lg={1}><button className="btn btn-primary" onClick={() => removePlayer(index, player)}>Delete</button></Col>
+    <Row className="player align-items-center">
+      <Col lg={1}><h3>{ordinal_suffix_of(index + 1)}</h3></Col>
+      <Col lg={4}><h3>{player.company}</h3></Col>
+      <Col lg={5}><h3><a href={'https://' + player.url} target="_blank" rel="noopener noreferrer">{player.url}</a></h3></Col>
+      <Col lg={1}><h3 className="text-right">{player.time}</h3></Col>
+      <Col lg={1} className="text-right"><button className="btn" onClick={() => removePlayer(index, player)}><span className="close-icon" aria-hidden="true">&times;</span></button></Col>
     </Row>
   )
 }
@@ -127,22 +151,35 @@ function App() {
   const [players, setPlayers] = useState([])
 
   useEffect(() => {
-    fetch('https://hidden-badlands-09770.herokuapp.com/get_players', {
+    fetch(`${live}/get_players`, {
       mode: 'cors',
     })
-    .then(res => res.json())
-    .then(data => {
-      setPlayers(data.sort((a, b) => a.time - b.time))
+      .then(res => res.json())
+      .then(data => {
+        setPlayers(data.sort((a, b) => a.time - b.time))
     })
   }, [])
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      fetch(`${live}/get_players`, {
+        mode: 'cors',
+      })
+      .then(res => res.json())
+      .then(data => {
+        setPlayers(data.sort((a, b) => a.time - b.time))
+      })
+    }, 1000)
+    return () => clearInterval(interval)
+  }, [])
+
   const addPlayer = state => {
-    const first = state.first, last= state.last, company = state.company, email = state.email, url = state.url, time = state.time
+    const { first, last, company, email, url, time } = state
 
     const newPlayer = [...players, {first, last, email, company, url, time}]
     newPlayer.sort((a, b) => a.time - b.time)
     setPlayers(newPlayer)
-    fetch('https://hidden-badlands-09770.herokuapp.com/new_player', {
+    fetch(`${live}/new_player`, {
       // mode: 'cors',
       method: 'POST',
       headers: {
@@ -156,31 +193,40 @@ function App() {
   const removePlayer = (index, player) => {
     const newPlayers = [...players]
     newPlayers.splice(index, 1)
-    fetch(`https://hidden-badlands-09770.herokuapp.com/player/${player.url}`, {
+    fetch(`${live}/player/${player.url}`, {
       method: 'DELETE'
     })
     setPlayers(newPlayers)
   }
 
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-    <Container className="App">
-      <Row className="header text-center align-items-center">
-        <Col lg={3}>
-          <Image className="logo" src={Logo} rounded/>
-        </Col>
-        <Col lg={6}>
-          <h4 className="title-small">legendarylion.com</h4><h1 className="title-large">#GETLOADED</h1>
-        </Col>
-        <Col lg={3}></Col>
-      </Row>
-      <Row className="text-center">
-        <Col lg={1}><h3 className="column-title">Rank</h3></Col>
-        <Col lg={4}><h3 className="column-title">Company</h3></Col>
-        <Col lg={5}><h3 className="column-title">Website</h3></Col>
-        <Col lg={1}><h3 className="column-title">Time</h3></Col>
-        <Col lg={1}></Col>
-      </Row>
-      <div className="loaded-list">
+    <div className="App">
+      <Container className="loaded-list h-100">
+        <Row className="header sticky-top align-items-end pb-3">
+          <Col lg={3}>
+            <Image className="logo" src={Logo} rounded />
+          </Col>
+          <Col lg={6}>
+            <h4 className="title-small">legendarylion.com</h4><h1 className="title-large">#GETLOADED</h1>
+          </Col>
+          <Col lg={3} className="text-right align-self-center">
+            <Button className="btn btn-lg btn-outline-light" variant="outline" onClick={handleShow}>Add New</Button>
+          </Col>
+          <Col lg={12}>
+            {/* <Row className="pt-5">
+              <Col lg={1}><h5 className="column-title">Rank</h5></Col>
+              <Col lg={4}><h5 className="column-title">Company</h5></Col>
+              <Col lg={5}><h5 className="column-title">Website</h5></Col>
+              <Col lg={1}><h5 className="column-title">Time</h5></Col>
+              <Col lg={1}></Col>
+            </Row> */}
+          </Col>
+        </Row>
         {players.map((player, index) => (
           <Player 
             key={index} 
@@ -189,9 +235,23 @@ function App() {
             removePlayer={removePlayer} 
           />
         ))}
-      </div>
-      <PlayerForm addPlayer={addPlayer} />
-    </Container>
+      </Container>
+      
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>New Score</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <PlayerForm addPlayer={addPlayer} />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleClose} type="submit" form="form">
+            Submit
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+    </div>
   );
 }
 
